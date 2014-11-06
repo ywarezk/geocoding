ae63651dd19b29a7e21c6a2d7238a2b735d954d1
ywarezk <ywarezk@gmail.com>
2014-11-06 23:22:26 +0200

    put mapbox geocoding

--- js/geocoding.js
+++ js/geocoding.js
@@ -24,7 +24,25 @@ function _init(){
         var place = autocomplete.getPlace();
         var table = prettyPrint(place);
         $('.google-geocoding .result').append(table);
-    });    
+    }); 
+    
+    //init mapbox geocoding
+    $("#mapbox-places").keyup(function() {
+        var val = $("#mapbox-places").val();
+        var url = 'http://api.tiles.mapbox.com/v4/geocode/mapbox.places-v1/' + val + '.json?access_token=pk.eyJ1IjoieXdhcmV6ayIsImEiOiJlVlU2eU9FIn0.yGTJwarqvLj1LMUv7oNh4w';
+        $.ajax({
+            url: url
+        }).done(function(data) {
+            var html = '<ul>';
+            for(var i=0; i<data.features.length; i++){
+                var singleFeature = data.features[i];
+                html = html + '<li>' + singleFeature.place_name + '</li>';        
+            }
+            html = html + '</ul>';
+            $('#autocomplete-mapbox').html(html);
+        });
+    });
+       
 };
 
 /***************************
